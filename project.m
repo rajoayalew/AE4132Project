1ddac78c2e53265a2e83b94453536e3e3ef0db8e
Raey Ayalew <raeyayalew@gmail.com>
2026-04-27 17:05:43 -0400

    Added node mapping

--- project.m
+++ project.m
@@ -6,8 +6,8 @@ close all
 
 innerRadius = 10;   % km
 outerRadius = 20;   % km
-numRadial = 1;      % number of radial divisions
-numTheta  = 20;     % number of angular divisions
+numRadial = 10;     % number of radial divisions
+numTheta  = 20;      % number of angular divisions
 
 %% Material Properties
 
@@ -15,28 +15,39 @@ sigma = 5.67 .* 10 .^ -8; % Stephan-Boltzman constant
 epsilon = 0.5;            % Emissivity of material 
 k = 21.9;                 % thermal conductivity (W/(m*K))
 
-%% Heating Properties
+%% Heat Properties
 
-Twall = 300;                          % Outer nodes have a known temperature
-Q  = 10000;                           % W/m^3
-qIn = 10;                             % heat flux in from nuclear plant (W/m^2)
-qOut = sigma .* epsilon .* Twall ^ 4; % heat flux out from radiation (W/m^2)
+Tinner = 10000;                        % Inner nodes 
+Touter = 300;                          % Outer nodes have a known temperature
+Q  = 10000;                            % W/m^3
+qIn = 10;                              % heat flux in from nuclear plant (W/m^2)
+qOut = sigma .* epsilon .* Touter ^ 4; % heat flux out from radiation (W/m^2)
 
-%% Plotting
+%% Mesh Creation
 
 % Angular divisions 
 theta = linspace(0, 2*pi, numTheta+1);
 
 % Radial divisions
-r2 = linspace(innerRadius^2, outerRadius^2, numRadial+1);
-r = sqrt(r2);
+r = sqrt(linspace(innerRadius^2, outerRadius^2, numRadial+1));
 
 % Create a mesh based on the following elements
 [Theta, R] = meshgrid(theta, r);
 X = R .* cos(Theta);
 Y = R .* sin(Theta);
 
-% Plot
+% Element Table
+nodes = [Y(:), X(:)];   % (N x 2) table of node coordinates in Cartesian
+nodeID = reshape(1:numel(X), size(X)); % For a given radial station i and theta location j, nodeID(i, j) returns the index in the nodes table 
+
+numNodes = size(nodes,1);
+numElements = numRadial * numTheta;
+
+elements = zeros(numElements, 4); 
+
+
+%% Plotting 
+
 figure;
 hold on;
 axis equal;
@@ -53,6 +64,7 @@ end
 
 xlabel('X (km)');
 ylabel('Y (km)');
+grid on, grid minor
 title('Equal-Area Mesh');
 
 hold off;
